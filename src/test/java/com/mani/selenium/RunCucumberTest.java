fe4c4efabff2b6b69e868efc7ee9cc059b4988ca
MANI <maninder1438@gmail.com>
2020-03-05 16:52:53 +0000

    new changes

--- src/test/java/com/mani/selenium/RunCucumberTest.java
+++ src/test/java/com/mani/selenium/RunCucumberTest.java
@@ -10,7 +10,6 @@ import org.junit.runner.RunWith;
         dryRun = false,                     //check the complete implementation of all the mentioned steps present in the Feature file. Before the execution starts . Dry Run is Checking the implementionation not about the execution  of scripts.
         strict = false,                     // if Strict option is set to true then at execution time if cucumber encounters any undefined/pending steps then cucumber does fails the execution and undefined steps are marked as fail and BUILD is FAILURE.
         plugin = {"json:target/cucumber.json"}  //it is used to create reports, here we supply the the path of json file
-
 )
 public class RunCucumberTest {
 }
