3aa2917e4dbd2db346e3bf71f854335b6b01ded8
maninder <maninder1438@gmail.com>
2020-03-07 21:12:22 +0000

    parallel test & selenium grid

--- src/test/java/com/mani/selenium/driver/DriverFactory.java
+++ src/test/java/com/mani/selenium/driver/DriverFactory.java
@@ -5,8 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class DriverFactory {
@@ -30,9 +34,22 @@ public class DriverFactory {
                 driver = new InternetExplorerDriver();
                 break;
             default:
+//                try {
+//                    remoteRun();
+//                } catch (MalformedURLException e) {
+//                    e.printStackTrace();
+//                }
                 WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
         }
+    }
+
+
+    public void remoteRun() throws MalformedURLException {
+
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setBrowserName("chrome");
+        driver = new RemoteWebDriver(new URL("http://192.168.1.143:4444/wd/hub"),cap);
     }
 
     public void maxBrowser() {
